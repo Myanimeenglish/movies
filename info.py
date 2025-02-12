71117dac23c9d874b7e1c40f74a4ac04f3709825
Myanimeenglish <myanimeenglishdub@gmail.com>
2025-02-12 10:27:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '28050588'))
 API_HASH = environ.get('API_HASH', '6ef31a2c5276a936ce2d2e228fa90fc3')
-BOT_TOKEN = environ.get('BOT_TOKEN', ' 7942472728:AAGBZcsPK70R8Bz-PfNecvf1RQ-7kuF6OZE')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7942472728:AAGBZcsPK70R8Bz-PfNecvf1RQ-7kuF6OZE')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7593547594').split()]
